b1626c4ae6c2825a09e0efa47401b22e90a9f2ab
ssoyeonnii <147587808+ssoyeonnii@users.noreply.github.com>
2025-05-26 23:14:31 +0900

    Feat : header에 모달창 클릭 이벤트 파라미터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import Calendar from "./components/Calendar.jsx";
+import Test from "./components/Test.jsx";
 
 function App() {
-  return <Calendar />;
+  // return <Calendar />;
+  return <Test />;
 }
 
 export default App;
