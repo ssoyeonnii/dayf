4e3b8a47ddeee3c8828140b0b7679831a41fbfad
soyeon <soyeon5447@gmail.com>
2025-06-28 23:49:30 +0900

    Refactor : 회원가입,로그인관련 router dom 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import "./App.css";
 import Calendar from "./components/Calendar.jsx";
 import Test from "./components/Test.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserJoin from "./components/UserJoin"; // 회원가입 페이지
+import UserLogin from "./components/UserLogin"; // 로그인 페이지
+import DeleteAccount from "./components/DeleteAccount"; // 회원탈퇴 페이지
 
 function App() {
-  // return <Calendar />;
-  return <Test />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Calendar />} />
+        <Route path="/UserJoin" element={<UserJoin />} />
+        <Route path="/UserLogin" element={<UserLogin />} />
+       <Route path="/DeleteAccount/:userId" element={<DeleteAccount />} />
+
+      </Routes>
+    </Router>
+  );
+  //return <Test />;
 }
 
 export default App;
